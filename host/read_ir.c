314852f1be1f6f77df8cdc2e36c8dc1f2a957588
Paul Fox <pgf@laptop.org>
2011-03-08 15:50:31 -0500

    add signal handling to restore termios on exit or kill, and use a smaller hammer when changing modes, so we're not in full raw mode.

--- host/read_ir.c
+++ host/read_ir.c
@@ -33,6 +33,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 #include <errno.h>
 #include <termios.h>
 #include <sys/ioctl.h>
@@ -48,57 +49,83 @@ usage(char *me)
     exit(1);
 }
 
+struct termios oldterm[1], newterm[1];
+int ir_fd;
+
+void
+restore_tty(void)
+{
+    tcsetattr(ir_fd, TCSAFLUSH, oldterm);
+}
+
+void
+restore_tty_sighandler(int n)
+{
+    restore_tty();
+    exit(1);
+}
+
 int
 main(int argc, char *argv[])
 {
-    int ir;
     unsigned char brk;
     int n, modemctl;
-    struct termios term[1];
 
     me = basename(argv[0]);
 
     if (argc == 1 || *argv[1] == '-')
 	usage(argv[0]);
 
-    ir = open(argv[1], O_RDONLY);
-    if (ir < 0) {
+    ir_fd = open(argv[1], O_RDONLY);
+    if (ir_fd < 0) {
 	fprintf(stderr, "%s: opening tty: %m\n", me);
 	exit(1);
     }
 
-    if (tcgetattr(ir, term)) {
+    if (tcgetattr(ir_fd, oldterm)) {
 	fprintf(stderr, "%s: getting attributes of tty: %m\n", me);
 	exit(1);
     }
 
+    *newterm = *oldterm;
+
+    atexit(restore_tty);
+    signal(SIGHUP, restore_tty_sighandler);
+    signal(SIGINT, restore_tty_sighandler);
+    signal(SIGQUIT, restore_tty_sighandler);
+    signal(SIGTERM, restore_tty_sighandler);
+
+
     /* be sure IGNBRK/BRKING/IGNPAR/PAMRK are all the way we
      * want them -- we just want a single '\0' character when
      * an rs232 BREAK occurs. */
-    cfmakeraw(term);
+    newterm->c_iflag &= ~(IGNBRK | BRKINT | IGNPAR | PARMRK);
+    newterm->c_lflag &= ~(ICANON);
 
     /*
      * at 1200 baud, a BREAK is a line assertion that lasts longer
      * than 4.17ms.  we expect the IR LED to assert for 10ms.
      */
-    if (cfsetspeed(term, B1200) || tcsetattr(ir, TCSANOW, term)) {
+    if (cfsetspeed(newterm, B1200) || tcsetattr(ir_fd, TCSANOW, newterm)) {
 	fprintf(stderr, "%s: setting attributes for tty: %m\n", me);
 	exit(1);
     }
 
     /* set up DTR and RTS with the right polarity */
-    if (ioctl(ir, TIOCMGET, &modemctl) ||
+    if (ioctl(ir_fd, TIOCMGET, &modemctl) ||
 	(modemctl &= ~TIOCM_RTS,
-	 modemctl |= TIOCM_DTR, ioctl(ir, TIOCMSET, &modemctl))) {
+	 modemctl |= TIOCM_DTR, ioctl(ir_fd, TIOCMSET, &modemctl))) {
 	fprintf(stderr, "%s: getting or setting RTS: %m\n", me);
-	exit(1);
+	// exit(1);
     }
 
+    setbuf(stdout, NULL);
 
     while (1) {
-	n = read(ir, &brk, 1);
+	n = read(ir_fd, &brk, 1);
 	if (n != 1) {
-	    fprintf(stderr, "Short or failed read from tty, quitting\n");
+	    fprintf(stderr, "Short or failed read from tty (%d), quitting\n",
+		    n);
 	    exit(1);
 	}
 	if (brk != 0) {
@@ -107,8 +134,7 @@ main(int argc, char *argv[])
 	    struct timeval tv;
 	    gettimeofday(&tv, 0);
 	    printf("s0x%lx u0x%lx\n",
-		(unsigned long)tv.tv_sec,
-		(unsigned long)tv.tv_usec);
+		   (unsigned long) tv.tv_sec, (unsigned long) tv.tv_usec);
 	}
     }
 
