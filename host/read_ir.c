f2ca2b0c3436ca965740794c457187fee5ec8175
Paul Fox <pgf@laptop.org>
2011-03-11 12:25:00 -0500

    add ability to test from a script, by allowing reads from non-tty.

--- host/read_ir.c
+++ host/read_ir.c
@@ -1,12 +1,5 @@
 /*
  *
- * This program interfaces to a simple detector circuit in order to
- * report infrared pulses emitted by many modern household electric
- * meters.  The detector circuit is wired such that it zeros the
- * rxdata line (pin 2) of the serial port.  This program detects
- * "break" (all-zero) conditions on a tty line, and reports their
- * occurrence by writing a timestamp to stdout.
- *
  * Copyright 2011 Paul Fox (pgf@foxharp.boston.ma.us)
  *
  * This program is free software; you can redistribute it and/or
@@ -23,6 +16,20 @@
  * License along with this program; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
+ * ---------------------
+ *
+ * This program interfaces to a simple detector circuit in order
+ * to report infrared pulses emitted by many modern household
+ * electric utility meters.  The detector circuit is wired such
+ * that it zeros the rxdata line (pin 2) of the serial port. 
+ * This program detects "break" (all-zero) conditions on the tty
+ * line (which are reported as characters numerically equal to 0), and reports their occurrence by writing a timestamp to
+ * stdout.
+ *
+ * Can do testing interactively:
+ *  read_ir /dev/tty
+ * (try using ctrl-SPACE' to generate a null character), or with a script:
+ *  while sleep 3; do echo -ne '\000'; done | read_ir -
  */
 
 #include <stdio.h>
@@ -45,17 +52,22 @@ char *me;
 void
 usage(char *me)
 {
-    fprintf(stderr, "%s: tty-name\n", me);
+    fprintf(stderr,
+	"usage: '%s tty-name', or\n"
+	" '%s -' to get nulls from stdin, but use\n"
+	" '%s /dev/tty' if testing interactively from a terminal\n", me, me, me);
     exit(1);
 }
 
 struct termios oldterm[1], newterm[1];
 int ir_fd;
+int is_tty;
 
 void
 restore_tty(void)
 {
-    tcsetattr(ir_fd, TCSAFLUSH, oldterm);
+    if (is_tty)
+	tcsetattr(ir_fd, TCSAFLUSH, oldterm);
 }
 
 void
@@ -73,52 +85,60 @@ main(int argc, char *argv[])
 
     me = basename(argv[0]);
 
-    if (argc == 1 || *argv[1] == '-')
+    if (argc == 1 || (argv[1][0] == '-' && argv[1][1] != '\0'))
 	usage(argv[0]);
 
-    ir_fd = open(argv[1], O_RDONLY);
-    if (ir_fd < 0) {
-	fprintf(stderr, "%s: opening tty: %m\n", me);
-	exit(1);
+    if (!strcmp(argv[1], "-")) {
+	is_tty = 0;
+	ir_fd = 0;
+    } else {
+	is_tty = 1;
+	ir_fd = open(argv[1], O_RDONLY);
+	if (ir_fd < 0) {
+	    fprintf(stderr, "%s: opening given tty: %m\n", me);
+	    exit(1);
+	}
     }
 
-    if (tcgetattr(ir_fd, oldterm)) {
-	fprintf(stderr, "%s: getting attributes of tty: %m\n", me);
-	exit(1);
-    }
+    if (is_tty) {
+	if (tcgetattr(ir_fd, oldterm)) {
+	    fprintf(stderr, "%s: getting attributes of tty: %m\n", me);
+	    exit(1);
+	}
 
-    *newterm = *oldterm;
+	*newterm = *oldterm;
 
-    atexit(restore_tty);
-    signal(SIGHUP, restore_tty_sighandler);
-    signal(SIGINT, restore_tty_sighandler);
-    signal(SIGQUIT, restore_tty_sighandler);
-    signal(SIGTERM, restore_tty_sighandler);
+	atexit(restore_tty);
+	signal(SIGHUP, restore_tty_sighandler);
+	signal(SIGINT, restore_tty_sighandler);
+	signal(SIGQUIT, restore_tty_sighandler);
+	signal(SIGTERM, restore_tty_sighandler);
 
 
-    /* be sure IGNBRK/BRKING/IGNPAR/PAMRK are all the way we
-     * want them -- we just want a single '\0' character when
-     * an rs232 BREAK occurs. */
-    newterm->c_iflag &= ~(IGNBRK | BRKINT | IGNPAR | PARMRK);
-    newterm->c_lflag &= ~(ICANON);
+	/* be sure IGNBRK/BRKING/IGNPAR/PAMRK are all the way we
+	 * want them -- we just want a single '\0' character when
+	 * an rs232 BREAK occurs. */
+	newterm->c_iflag &= ~(IGNBRK | BRKINT | IGNPAR | PARMRK);
+	newterm->c_lflag &= ~(ICANON);
 
-    /*
-     * at 1200 baud, a BREAK is a line assertion that lasts longer
-     * than 4.17ms.  we expect the IR LED to assert for 10ms.
-     */
-    if (cfsetspeed(newterm, B38400) || tcsetattr(ir_fd, TCSANOW, newterm)) {
-	fprintf(stderr, "%s: setting attributes for tty: %m\n", me);
-	exit(1);
-    }
+	/*
+	 * at 1200 baud, a BREAK is a line assertion that lasts longer
+	 * than 4.17ms.  we expect the IR LED to assert for 10ms.
+	 */
+	if (cfsetspeed(newterm, B38400) || tcsetattr(ir_fd, TCSANOW, newterm)) {
+	    fprintf(stderr, "%s: setting attributes for tty: %m\n", me);
+	    exit(1);
+	}
 
-    /* set up DTR and RTS with the right polarity:  disable RTS and
-     * enable DTR to leave RTS negative and DTR positive.
-     */
-    if (ioctl(ir_fd, TIOCMGET, &modemctl) ||
-	(modemctl &= ~TIOCM_RTS,
-	 modemctl |= TIOCM_DTR, ioctl(ir_fd, TIOCMSET, &modemctl))) {
-	fprintf(stderr, "%s: getting or setting RTS: %m\n", me);
-	// exit(1);
+	/* set up DTR and RTS with the right polarity:  disable RTS and
+	 * enable DTR to leave RTS negative and DTR positive.
+	 */
+	if (ioctl(ir_fd, TIOCMGET, &modemctl) ||
+	    (modemctl &= ~TIOCM_RTS,
+	     modemctl |= TIOCM_DTR, ioctl(ir_fd, TIOCMSET, &modemctl))) {
+	    fprintf(stderr, "%s: getting or setting RTS: %m\n", me);
+	    // exit(1);
+	}
     }
 
     setlinebuf(stdout);
